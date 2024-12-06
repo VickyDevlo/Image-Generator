a6a1339b7d41e1b13a3b0b553f6df2e5b23628d9
vickyDevlo <vickymagare1996@gmail.com>
2024-12-06 15:16:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() { 
+
+  return (
+     <div>
+      <h1 className="text-3xl text-center py-8 text-gray-600">AI SaaS Image-Generator</h1>
+     </div>
+  )
+}
+
+export default App
